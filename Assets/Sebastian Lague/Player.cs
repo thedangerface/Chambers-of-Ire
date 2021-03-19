674a66937e83989e953ef2a06293487257293427
DangerFace <dangerface@mindwyrmgames.com>
2021-03-18 23:05:24 -0500

    halfway through implementing the Lague Raycast 2D controller

---
+++ Assets/Sebastian Lague/Player.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (Controller2D))]
+public class Player : MonoBehaviour
+{
+    public float jumpHeight = 4;
+    public float timeToJumpApex = .4f;
+    float accelerationTimeAirborne = .2f;
+    float accelerationTimeGrounded = .1f;
+    float moveSpeed = 6;
+
+    float gravity;
+    float jumpVelocity;
+    Vector3 velocity;
+    float velocityXSmoothing;
+
+    Controller2D controller;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<Controller2D>();
+
+        gravity = -(2 * jumpHeight) / Mathf.Pow(timeToJumpApex, 2);
+        jumpVelocity = Mathf.Abs(gravity * timeToJumpApex);
+        print("Gravity: " + gravity + " Jump Velocity: " + jumpVelocity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (controller.collisions.above || controller.collisions.below)
+        {
+            velocity.y = 0;
+        }
+        
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        //jump controls
+
+        if (Input.GetKeyDown(KeyCode.Space) && controller.collisions.below)
+        {
+            velocity.y = jumpVelocity;
+            //print("space pressed at " + Time.time);
+        }
+
+        // if you want to control the jump so that you start falling as soon as you let go
+        // you will need to reset the player's velocity to zero
+
+        if (Input.GetKeyUp(KeyCode.Space) && !controller.collisions.below && velocity.y > 0)
+        {
+            velocity.y = 0;
+            // need a way to briefly and accurate boost downward speed
+            // maybe log jump start time? calculate speed based off how much time you have to fall.
+            // look at the calculations for jump height
+            // don't forget to compensate for gravity's pull
+
+            //print("space released at " + Time.time);
+        }
+
+        float targetVelocityX = input.x * moveSpeed;
+        velocity.x = Mathf.SmoothDamp(velocity.x, targetVelocityX, ref velocityXSmoothing, 
+            (controller.collisions.below)?accelerationTimeGrounded:accelerationTimeAirborne);
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
---
+++ Assets/Sebastian Lague/RaycastController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class RaycastController : MonoBehaviour
+{
+    public LayerMask collisionMask;
+
+    public const float skinWidth = .015f;
+    public int horizontalRayCount = 4;
+    public int verticalRayCount = 4;
+
+    [HideInInspector]
+    public float horizontalRaySpacing;
+    [HideInInspector]
+    public float verticalRaySpacing;
+
+    [HideInInspector]
+    public BoxCollider2D _collider2D;
+    [HideInInspector]
+    public RaycastOrigins raycastOrigins;
+
+    // Start is called before the first frame update
+    public virtual void Start()
+    {
+        _collider2D = GetComponent<BoxCollider2D>();
+        CalculateRaySpacing();
+    }
+
+    // Update is called once per frame
+    public void UpdateRaycastOrigins()
+    {
+        Bounds bounds = _collider2D.bounds;
+        bounds.Expand(skinWidth * -2);
+
+        raycastOrigins.bottomLeft = new Vector2(bounds.min.x, bounds.min.y);
+        raycastOrigins.bottomRight = new Vector2(bounds.max.x, bounds.min.y);
+        raycastOrigins.topLeft = new Vector2(bounds.min.x, bounds.max.y);
+        raycastOrigins.topRight = new Vector2(bounds.max.x, bounds.max.y);
+    }
+
+    public void CalculateRaySpacing()
+    {
+        Bounds bounds = _collider2D.bounds;
+        bounds.Expand(skinWidth * -2);
+
+        horizontalRayCount = Mathf.Clamp(horizontalRayCount, 2, int.MaxValue);
+        verticalRayCount = Mathf.Clamp(verticalRayCount, 2, int.MaxValue);
+
+        horizontalRaySpacing = bounds.size.y / (horizontalRayCount - 1);
+        verticalRaySpacing = bounds.size.x / (verticalRayCount - 1);
+    }
+
+    public struct RaycastOrigins
+    {
+        public Vector2 topLeft, topRight;
+        public Vector2 bottomLeft, bottomRight;
+    }
+}
