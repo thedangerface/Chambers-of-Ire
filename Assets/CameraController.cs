32b0f7a66a78e3a60bc3ad96aaeb09d727955cfd
DangerFace <dangerface@mindwyrmgames.com>
2021-03-27 13:39:18 -0500

    added healthbar and portrait to level 1. added level restart when falling through ground

---
+++ Assets/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float xMin, xMax, yMin, yMax;
+    [SerializeField] GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float targetX = Mathf.Clamp(player.transform.position.x, xMin, xMax);
+        transform.position = new Vector3(targetX, transform.position.y, transform.position.z);
+    }
+}
---
+++ Assets/PlayerData.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerData : MonoBehaviour
+{
+    public int health = 8, healthMax = 8;
+}
