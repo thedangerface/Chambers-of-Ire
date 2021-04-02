d0e439e235705d640b56e1e02d761b282dc01d2c
nickthorpe71 <nickthorpe71@gmail.com>
2021-04-02 07:30:31 -0500

    adds attack input to player input, adds ability filtering

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -35,6 +35,13 @@ public class Player : MonoBehaviour
     bool wallSliding;
     int wallDirectionX;
 
+    [Header("Abilities")]
+    public bool canMove = true;
+    public bool canJump = true;
+    public bool canWallJump = true;
+    public bool canDoubleJump = true;
+    public bool canAttack = true;
+
     PlayerAnimationManager playerAnimationManager;
 
     void Start()
@@ -55,7 +62,8 @@ public class Player : MonoBehaviour
         CalculateVelocity();
         HandleWallSliding();
 
-        controller.Move(velocity * Time.deltaTime, directionalInput);
+        if(canMove)
+            controller.Move(velocity * Time.deltaTime, directionalInput);
 
         if (controller.collisions.above || controller.collisions.below)
         {
@@ -68,12 +76,18 @@ public class Player : MonoBehaviour
                 velocity.y = 0;
             }
         }
+    }
 
-        // ATTACK INPUT
-        if (Input.GetKeyDown(KeyCode.Keypad0))
-        {
+    public void OnAttackInputDown()
+    {
+        if(canAttack)
             playerAnimationManager.stateInfo.attacking = true;
-        }
+    }
+
+    public void OnAttackInputUp()
+    {
+        if(canAttack)
+            playerAnimationManager.stateInfo.attacking = false;
     }
 
     public void SetDirectionalInput(Vector2 input)
@@ -83,37 +97,40 @@ public class Player : MonoBehaviour
 
     public void OnJumpInputDown()
     {
-        if (wallSliding)
+        if(canJump)
         {
-            if (wallDirectionX == directionalInput.x)
+            if (wallSliding && canWallJump)
             {
-                velocity.x = -wallDirectionX * wallJumpClimb.x;
-                velocity.y = wallJumpClimb.y;
-            }
-            else if (directionalInput.x == 0)
-            {
-                velocity.x = -wallDirectionX * wallJumpOff.x;
-                velocity.y = wallJumpOff.y;
-            }
-            else
-            {
-                velocity.x = -wallDirectionX * wallLeap.x;
-                velocity.y = wallLeap.y;
-            }
-        }
-        if (controller.collisions.below)
-        {
-            if (controller.collisions.slidingDownMaxSlope)
-            {
-                if (directionalInput.x != -Mathf.Sign(controller.collisions.slopeNormal.x))
-                { // not jumping against max slope
-                    velocity.y = maxJumpVelocity * controller.collisions.slopeNormal.y;
-                    velocity.x = maxJumpVelocity * controller.collisions.slopeNormal.x;
+                if (wallDirectionX == directionalInput.x)
+                {
+                    velocity.x = -wallDirectionX * wallJumpClimb.x;
+                    velocity.y = wallJumpClimb.y;
+                }
+                else if (directionalInput.x == 0)
+                {
+                    velocity.x = -wallDirectionX * wallJumpOff.x;
+                    velocity.y = wallJumpOff.y;
+                }
+                else
+                {
+                    velocity.x = -wallDirectionX * wallLeap.x;
+                    velocity.y = wallLeap.y;
                 }
             }
-            else
+            if (controller.collisions.below)
             {
-                velocity.y = maxJumpVelocity;
+                if (controller.collisions.slidingDownMaxSlope)
+                {
+                    if (directionalInput.x != -Mathf.Sign(controller.collisions.slopeNormal.x))
+                    { // not jumping against max slope
+                        velocity.y = maxJumpVelocity * controller.collisions.slopeNormal.y;
+                        velocity.x = maxJumpVelocity * controller.collisions.slopeNormal.x;
+                    }
+                }
+                else
+                {
+                    velocity.y = maxJumpVelocity;
+                }
             }
         }
     }
--- Assets/Scripts/Player/PlayerInput.cs
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -12,26 +12,26 @@ public class PlayerInput : MonoBehaviour
 
     void Update()
     {
+        // Movement
         Vector2 directionalInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         player.SetDirectionalInput(directionalInput);
 
+        // Run
         if(Input.GetMouseButtonDown(1))
-        {
             player.OnRunInputDown();
-        }
-
         if(Input.GetMouseButtonUp(1))
-        {
             player.OnRunInputUp();
-        }
 
-        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
-        {
+        // Jump
+        if (Input.GetMouseButtonDown(0))
             player.OnJumpInputDown();
-        }
-        if (Input.GetKeyUp(KeyCode.Space) || Input.GetMouseButtonUp(0))
-        {
+        if (Input.GetMouseButtonUp(0))
             player.OnJumpInputUp();
-        }
+        
+        // Attack
+        if (Input.GetKeyDown(KeyCode.Space))
+            player.OnAttackInputDown();
+        if (Input.GetKeyUp(KeyCode.Space))
+            player.OnAttackInputUp();
     }
 }
