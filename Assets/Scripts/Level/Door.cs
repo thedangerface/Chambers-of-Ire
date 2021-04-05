be421af2c5eb02e8eb7412f830faaafecdfd97d9
DangerFace <dangerface@mindwyrmgames.com>
2021-04-04 21:33:52 -0500

    added basic door mechanic. first part of tutorial roughed in

---
+++ Assets/Scripts/Level/Door.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Collider2D))]
+public class Door : MonoBehaviour
+{
+    [SerializeField] Transform exit;
+    bool moving;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            //run coroutine to trigger animation, then move player
+            collision.gameObject.transform.position = exit.transform.position;
+            moving = true;
+        }
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player" && moving == false)
+        {
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                //run coroutine to trigger animation, then move player
+                collision.gameObject.transform.position = exit.transform.position;
+                moving = true;
+            }
+        }
+    }
+
+}
